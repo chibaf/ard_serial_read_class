c01ba3ff6ffa388bef2be614bbfff2bbc2d9cb92
chibaf <chibaf@mac.com>
2022-09-09 10:13:51 +0900

    Add files via upload

--- ard_serial_read_class.py
+++ ard_serial_read_class.py
@@ -3,10 +3,11 @@ class ard_serial_read:
   def read_serial(self,ser):
     import serial
     line = ser.readline()
-    try:
-      line2=line.strip().decode('utf-8')
-    except UnicodeDecodeError:
-      print("decode error")
-      exit()
+    while True:
+      try:
+        line2=line.strip().decode('utf-8')
+        break
+      except UnicodeDecodeError:
+        continue
     data = [float(val) for val in line2.split(",")]
     return data
